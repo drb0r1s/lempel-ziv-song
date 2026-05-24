2ec0817891d67dd445414ba74e0219228d33e0ad
Nikola Gostovikj <nikolagostovic8@gmail.com>
2026-05-24 02:25:30 +0200

    Java future implementation

---
+++ lzwjava/src/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.file.*;
+import java.util.*;
+
+public class Main {
+    static final int MAX_CODE = 4095; //2^12-1
+    static final int CODE_BITS = 12;
+    public static void main(String[] args) {
+
+    }
+}
